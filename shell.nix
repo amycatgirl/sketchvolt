85e9eb5129377082262f74876c534b08d7319790
Amy <amycatgirl@disroot.org>
2023-12-24 11:46:17 -0400

    chore: Pin packages

--- shell.nix
+++ shell.nix
@@ -1,4 +1,4 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/d6863cbcbbb80e71cecfc03356db1cda38919523.tar.gz") {} }:
 pkgs.mkShell {
     name = "JavascriptPluginBoilerplateShell";
     buildInputs = with pkgs; [
